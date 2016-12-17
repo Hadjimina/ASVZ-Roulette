2964c10d6710ba073bcd92c0cd36fbac1652fa0c
david <davidn@student.ethz.ch>
2016-12-17 10:23:52 +0100

    Implemented signup and login function.

--- app/src/main/java/com/example/philipp/asvzroulette/LoginActivity.java
+++ app/src/main/java/com/example/philipp/asvzroulette/LoginActivity.java
@@ -2,19 +2,48 @@ package com.example.philipp.asvzroulette;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.provider.ContactsContract;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.GenericTypeIndicator;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.HashMap;
+import java.util.List;
 
 public class LoginActivity extends AppCompatActivity {
 
-    private Button signup,skip;
+    private Button signup,skip,login;
+    private TextView loginText, errorEmail, errorPassword;
+    private EditText email, password;
+    private String stringEmail, stringPassword;
+    private DatabaseReference dbReference;
+
+
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activitylogin);
 
+        loginText = (TextView) findViewById(R.id.loginText);
+        email = (EditText) findViewById(R.id.email);
+        password = (EditText) findViewById(R.id.password);
+        errorEmail = (TextView) findViewById(R.id.errorEmailLogin);
+        errorPassword = (TextView) findViewById(R.id.errorPasswordLogin);
+
+        login = (Button) findViewById(R.id.login);
         signup = (Button) findViewById(R.id.signup);
         skip = (Button) findViewById(R.id.skip);
 
@@ -31,5 +60,69 @@ public class LoginActivity extends AppCompatActivity {
                 startActivity(new Intent(v.getContext(), MainActivity.class));
             }
         });
+
+        login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(final View x) {
+                //get user input
+                stringEmail = email.getText().toString();
+                stringPassword = password.getText().toString();
+                errorEmail.setVisibility(View.INVISIBLE);
+                errorPassword.setVisibility(View.INVISIBLE);
+
+                //database update
+                dbReference  = FirebaseDatabase.getInstance().getReference();
+                try{
+
+                    dbReference.child("Users").child(Long.toString(getAscii(stringEmail))).child("password").addListenerForSingleValueEvent(new ValueEventListener() {
+                        @Override
+                        public void onDataChange(DataSnapshot snapshot) {
+                            int i = 0;
+                           String [] passwordInfo = new String[2];
+                            for (DataSnapshot snap : snapshot.getChildren()){
+                                String s = snap.getValue(String.class);
+                                passwordInfo[i] = s;
+                                i++;
+                            }
+                            try{
+                            HashedPassword stringPasswordHash = new HashedPassword(stringPassword, passwordInfo[1]);
+
+
+                                if (passwordInfo[0].equals(stringPasswordHash.getHashedPassword())) {
+                                    startActivity(new Intent(x.getContext(), MainActivity.class));
+
+                                } else {
+                                    errorPassword.setVisibility(View.VISIBLE);
+                                }
+                            }
+                            catch(java.lang.NullPointerException e){
+                                errorEmail.setVisibility(View.VISIBLE);
+
+                            }
+
+
+                        }
+                        @Override
+                        public void onCancelled(DatabaseError error) {
+                        }
+                    });
+                }
+
+                catch (com.google.firebase.database.DatabaseException e){
+                    errorEmail.setVisibility(View.VISIBLE);
+                }
+
+
+            }
+        });
     }
+    private long getAscii(String s){
+        long sum=0;
+        for (int i=0; i<s.length(); i++){
+            sum += (long) s.charAt(i);
+        }
+        System.out.println(sum);
+        return sum;
+    }
+
 }
--- app/src/main/java/com/example/philipp/asvzroulette/SignupActivity.java
+++ app/src/main/java/com/example/philipp/asvzroulette/SignupActivity.java
@@ -1,9 +1,39 @@
 package com.example.philipp.asvzroulette;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+import com.google.firebase.database.DatabaseError;
 
 public class SignupActivity extends AppCompatActivity {
+
+    private DatabaseReference dbReference;
+    private TextView textView;
+    private TextView errorForename;
+    private TextView errorSurname;
+    private TextView errorPassword1;
+    private TextView errorPassword2;
+    private EditText forename;
+    private EditText surname;
+    private EditText email;
+    private EditText password;
+    private EditText checkPassword;
+    private Button submit;
+    public Boolean noErrors;
+    private Long userCount;
+    private User user;
+
+
+
 
 
     //Dave do your thing
@@ -11,5 +41,141 @@ public class SignupActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_signup);
+
+        //setup of all components
+        textView = (TextView) findViewById(R.id.new_account);
+
+        forename = (EditText) findViewById(R.id.editVorname);
+
+        surname = (EditText) findViewById(R.id.editName);
+
+        password = (EditText) findViewById(R.id.editPassword);
+
+        checkPassword = (EditText) findViewById(R.id.editCheckPassword);
+
+        email = (EditText) findViewById(R.id.editEmail);
+
+        errorForename = (TextView) findViewById(R.id.errorForename);
+
+        errorSurname = (TextView) findViewById(R.id.errorSurname);
+
+        errorPassword1 = (TextView) findViewById(R.id.errorPassword);
+
+        errorPassword2 = (TextView) findViewById(R.id.errorPassword2);
+
+
+        submit = (Button) findViewById(R.id.button);
+        submit.setOnClickListener(new View.OnClickListener(){
+            public void onClick(View v){
+                //set all error messages invisible
+                errorForename.setVisibility(View.INVISIBLE);
+                errorPassword1.setVisibility(View.INVISIBLE);
+                errorPassword2.setVisibility(View.INVISIBLE);
+                errorSurname.setVisibility(View.INVISIBLE);
+                noErrors = true;
+
+                //get user input
+                String forenameString = forename.getText().toString();
+                String surnameString = surname.getText().toString();
+                String passwordString = password.getText().toString();
+                String checkPasswordString = checkPassword.getText().toString();
+                final String emailString = email.getText().toString();
+
+                //input verification and error message displaying
+                if(passwordString.length() > 20){
+                    errorPassword2.setVisibility(View.VISIBLE);
+                    noErrors = false;
+                }
+
+                if(isNameTooLong(forenameString)){
+                    errorForename.setVisibility(View.VISIBLE);
+                    noErrors = false;
+                }
+
+                if(isNameTooLong(surnameString)){
+                    errorSurname.setVisibility(View.VISIBLE);
+                    noErrors = false;
+                }
+
+                if(!arePasswordsEqual(passwordString, checkPasswordString)){
+                    errorPassword1.setVisibility(View.VISIBLE);
+                    noErrors = false;
+                }
+
+                //Load the data to the database.
+                if(noErrors){
+                    HashedPassword passwordHash = new HashedPassword(passwordString); //generate the password hash
+                    user = new User(forenameString, surnameString, emailString, passwordHash); //new User
+                    dbReference  = FirebaseDatabase.getInstance().getReference();
+                    DatabaseReference countRef = dbReference.child("userCount");
+
+                    countRef.addListenerForSingleValueEvent(new ValueEventListener() {
+                        //get the current number of users
+                        @Override
+                        public void onDataChange(DataSnapshot snapshot) {
+                            userCount=(snapshot.getValue(Long.class));
+
+
+                        }
+                        @Override public void onCancelled(DatabaseError error) {}
+                    });
+                    dbReference.addListenerForSingleValueEvent(new ValueEventListener() {
+                        @Override
+                        public void onDataChange(DataSnapshot dataSnapshot) {
+                            user.setUserID((userCount)+1);
+
+
+
+                            dbReference.child("userCount").setValue(userCount+1);    //increase user count in database
+                            dbReference.child("Users").child(Long.toString(getAscii(emailString))).setValue(user);//load new user information to database.
+                        }
+
+                        @Override
+                        public void onCancelled(DatabaseError databaseError) {
+
+                        }
+                    });
+                    startActivity(new Intent(v.getContext(), LoginActivity.class));
+
+                }
+
+
+
+            }
+        });
+
+
+
+
+
     }
+
+    //Check if inputs aren't too long
+    private boolean isNameTooLong(String s){
+        return s.length() > 30;
+    }
+
+
+    //Check if password inputs are equal.
+    private boolean arePasswordsEqual(String p1, String p2){
+        if (p1.length() != p2.length()){
+            return false;
+        }
+        for (int i = 0; i < p1.length(); i++){
+            if (p1.charAt(i) != p2.charAt(i)){
+                return false;
+            }
+        }
+        return true;
+    }
+
+    private long getAscii(String s){
+        long sum=0;
+        for (int i=0; i<s.length(); i++){
+            sum += (long) s.charAt(i);
+        }
+        return sum;
+    }
+
+
 }
